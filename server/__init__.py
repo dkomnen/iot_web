90f604fda02cd46efb3bf997e52a0f7bb38dc39f
dkomnen <davidkomljenovic93@gmail.com>
2018-02-20 16:29:14 +0100

    Add template for index page and css

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 api = Api(app)
 
 from controllers import ApiController, ViewController
