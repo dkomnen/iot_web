86205bc92e1fa723ed01e67e2bd980795f23a6f3
dkomnen <davidkomljenovic93@gmail.com>
2018-06-03 21:10:50 +0200

    add mqtt, refactor power and water pages

--- mqtt/MqttClient.py
+++ mqtt/MqttClient.py
@@ -1,6 +1,9 @@
 from paho.mqtt.client import *
+from proto import message_pb2
 
 print "MQTT CLIENT SCRIPT"
+
+
 # # The callback for when the client receives a CONNACK response from the server.
 # def on_connect(client, userdata, flags, rc):
 #     print("Connected with result code "+str(rc))
@@ -12,9 +15,15 @@ print "MQTT CLIENT SCRIPT"
 # # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     # print(msg.topic+" "+str(msg.payload))
-    if (msg.topic == "test-topic"):
+    if msg.topic == "test-topic":
+        received_message = message_pb2.DeviceReading()
         print "A message was received from the test-topic"
-        print str(msg.payload.decode("utf-8"))
+        received_message.ParseFromString(msg.payload)
+        print received_message
+
+    if msg.topic == "online":
+        print "LMAO"
+        print msg.payload
 
 
 #
@@ -40,5 +49,15 @@ client.on_message = on_message
 client.loop_start()
 
 client.subscribe("test-topic")
+client.subscribe("online")
 
-client.publish("test-topic", "Whatever")
+# -----
+message = message_pb2.DeviceReading()
+message.sensor_type = "temperature"
+message.sensor_reading = 20
+message.unit = "c"
+message.serial_number = "1"
+# -----
+
+client.publish("test-topic", message.SerializeToString())
+#client.publish("52:7d:e6:ef:2a:8b/remote_shutdown", "test")
